42bad083abec002485408975779d0ff66e9d43a1
maikel abdelmalak <maikel22@hotmail.it>
2020-05-06 18:30:30 +0200

    1

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-//setto timeout per comparsa div privacy
-setTimeout(function () {
-    $('.privacy').slideDown(500);
-}, 2000);
+$(document).ready(function(){
+    //setto timeout per comparsa div privacy
+    setTimeout(function () {
+        $('.privacy').slideDown(500);
+    }, 2000);
 
-//intercetto click sui due bottoni per chiudere div privacy
-$('.privacy button').click(function(){
-    $('.privacy').slideUp(500);
-})
+    //intercetto click sui due bottoni per chiudere div privacy
+    $('.privacy button').click(function(){
+        $('.privacy').slideUp(500);
+    });
+
+
+    $('.grill').click(function(){
+        if($("#dropdown").hasClass("hidden")){
+            $('#dropdown').removeClass('hidden');
+        }else{
+            $('#dropdown').addClass('hidden');
+        }
+    });
+
+//     $(document).click(function(event){
+//             var target = $(event.target);
+//             if(target.hasClass('grill')){
+//                 if($("#dropdown").hasClass("hidden")){
+//                     $('#dropdown').removeClass('hidden');
+//                 }else{
+//                     $('#dropdown').addClass('hidden');
+//                 }
+//             }else{
+//                 $('#dropdown').addClass('hidden');
+//             }
+//
+//     });
+});
